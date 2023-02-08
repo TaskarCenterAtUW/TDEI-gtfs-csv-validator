6a8baa575ca3bfc493b02247155c68daaa4f71fc
Kristin Ann Tufte <katuw@uw.edu>
2023-02-07 20:06:32 -0800

    minor fix to example_use

--- tdei_gtfs_csv_validator/example_use.py
+++ tdei_gtfs_csv_validator/example_use.py
@@ -5,13 +5,12 @@ from tdei_gtfs_csv_validator import exceptions as gcvex
 
 data_type = 'gtfs_pathways'
 schema_version = 'v1.0'
-#path = 'PUT PATH TO ZIP FILE OR DIRECTORY HERE'
-path = '/Users/kristintufte/Projects/test-package/mytests/test_files/gtfs_pathways/v1.0/success_1_all_attrs.zip'
+path = 'PUT PATH TO ZIP FILE OR DIRECTORY HERE'
 
 print("simple_test: trying calling test_release")
 
 try:
-    gcv_test_release.test_release(data_type, schema_version, dir_path)
+    gcv_test_release.test_release(data_type, schema_version, path)
 except gcvex.GCVError as err:
     print("Test Failed\n")
     print(err) 
